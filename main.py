46cb380654d3c4d652a48b8c1419ba805e2cba39
bobo <77556196+opalgreenbranch@users.noreply.github.com>
2021-04-06 18:25:55 -0500

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,41 +1,37 @@
-let i: number;
 led.plot(0, 0)
-let x = 0
-let y = 0
-for (i = 0; i < 4; i++) {
+x = 0
+y = 0
+
+for i in range(4):
     y += 1
     x += 1
     led.plot(x, y)
-    basic.pause(200)
-}
+    basic.pause(200)    
+
 y = 0
-for (i = 0; i < 4; i++) {
+for i in range(4):
     led.plot(x, y)
     y += 1
     x -= 1
     basic.pause(200)
-}
-for (i = 0; i < 4; i++) {
+
+for i in range(4):
     led.plot(x, y)
     y -= 1
     basic.pause(200)
-}
-for (i = 0; i < 4; i++) {
+for i in range(4):
     x += 1
     led.plot(x, y)
     basic.pause(200)
-}
-for (i = 0; i < 4; i++) {
+for i in range(4):
     y += 1
     led.plot(x, y)
     basic.pause(200)
-}
-for (i = 0; i < 4; i++) {
+for i in range(4):
     x -= 1
     led.plot(x, y)
     basic.pause(200)
-}
 basic.pause(2000)
-basic.clearScreen()
+basic.clear_screen()
 basic.pause(500)
-basic.showString("Done! Created by opalgreenbranch with Python.")
+basic.show_string("Done! Created by opalgreenbranch with Python.")
